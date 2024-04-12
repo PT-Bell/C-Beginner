d64eb38cc293305b91f1392ac283fd46282521ab
PTBell <l2yoonjong@gmail.com>
2024-04-12 09:06:56 +0900

    7568 text commit

--- C_beginner/Baekjoon.c
+++ C_beginner/Baekjoon.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-int main(){
-    int n = 0;
-    scanf("%d", &n);
 
+int main(){
+    int N = 0;
+    int* weight;
+    int* height;
+
+    scanf("%d", &N);
+
+    int* weight = (int*)malloc(sizeof(int) * N);
+    int* height = (int*)malloc(sizeof(int) * N);
+
+    for(int i = 0; i < N; i++){
+        scanf("%d %d", weight[i], height[i]);
+    }
+
+    for(int i = 0; i < N; i++){
+        printf("%d %d", weight[i], height[i]);
+    }
     return 0;
 }
